e1a3f71cbf5ac172e56b517adef999af44691edb
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2018-12-21 22:02:11 -0300

    32. Barra de progreso y spinner

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -5,6 +5,7 @@ import Title from '../components/title'
 import PlayPause from '../components/play-pause'
 import Timer from '../components/timer'
 import Controls from '../components/video-player-controls'
+import ProgressBar from '../components/progress-bar'
 
 class VideoPlayer extends Component {
   state = {
@@ -38,6 +39,10 @@ class VideoPlayer extends Component {
     })
   }
 
+  handleProgressChange = event => {
+    this.video.currentTime = event.target.value
+  }
+
   render () {
     return (
       <VideoPlayerLayout>
@@ -52,6 +57,11 @@ class VideoPlayer extends Component {
           <Timer
             currentTime={this.state.currentTime}
             duration={this.state.duration}
+          />
+          <ProgressBar
+            duration={this.state.duration}
+            value={this.state.currentTime}
+            handleProgressChange={this.handleProgressChange}
           />
         </Controls>
         <Video
